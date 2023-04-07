ba76a0a5fecc99efaaf837ff2d53d0942cbd8982
진환 정 <joung.jinhawn@gmail.com>
2023-04-07 19:27:50 +0900

    1차 완성, 디버깅 필요, 주석 필요 (23.04.07)

--- CalculatePrice.ts
+++ CalculatePrice.ts
@@ -20,19 +20,74 @@ export class CalculatePrice {
         this.docBID = JSON.parse(bidString);
         this.eleBID = this.docBID['data'];
 
-        this.ApplyStandardPriceOption();
+        this.Reset();
 
-        // this.myPercent = Big(0.85)
+        if(Data.StandardMarketDeduction.localeCompare('1'))
+            this.ApplyStandardPriceOption();
 
-        // this.CalculateRate(-1, 2.9932);
+        this.GetFixedPriceRate();
+        this.FindMyPercent();
+        this.GetWeight();
+        this.CalculateRate(Data.PersonalRate, Data.BalancedRate);
+        this.Recalculation();
 
-        // this.SetBusinessInfo();
+        if(this.exCount !== 0){
+            this.SetExcludingPrice();
+            this.GetAdjustedExcludePrice();
+        }
 
-        //this.CreateFile();
+        this.SetPriceOfSuperConstruction();
+
+        FillCostAccount.CalculateBiddingCosts();
+        this.SetBusinessInfo();
+        this.SubstitutePrice();
+        this.CreateFile();
     }
 
     public static Reset(): void {
+        Data.ExecuteReset = '1';
 
+        let DM: number = Data.Investigation['직접재료비'];
+        let DL: number = Data.Investigation['직접노무비'];
+        let OE: number = Data.Investigation['산출경비'];
+        let FM: number = Data.InvestigateFixedPriceDirectMaterial;
+        let FL: number = Data.InvestigateFixedPriceDirectLabor;
+        let FOE: number = Data.InvestigateFixedPriceOutputExpense;
+        let SM: number = Data.InvestigateStandardMaterial;
+        let SL: number = Data.InvestigateStandardLabor;
+        let SOE: number = Data.InvestigateStandardExpense;
+
+        Data.RealDirectMaterial = DM;
+        Data.RealDirectLabor = DL;
+        Data.RealOutputExpense = OE;
+        Data.FixedPriceDirectMaterial = FM;
+        Data.FixedPriceDirectLabor = FL;
+        Data.FixedPriceOutputExpense = FOE;
+        Data.StandardMaterial = SM;
+        Data.StandardLabor = SL;
+        Data.StandardExpense = SOE;
+
+        const bidT3: object = this.eleBID['T3'];
+        let code: string;
+        let type: string;
+
+        for (let key in bidT3) {
+            code = JSON.stringify(bidT3[key]['C9']['_text']);
+            type = JSON.stringify(bidT3[key]['C5']['_text'])[1];
+
+            if (code !== undefined && type === 'S') {
+                let constNum: string = JSON.stringify(bidT3[key]['C1']['_text']).slice(1, -1);
+                let numVal: string = JSON.stringify(bidT3[key]['C2']['_text']).slice(1, -1);
+                let detailVal: string = JSON.stringify(bidT3[key]['C3']['_text']).slice(1, -1);
+                let curObject = Data.Dic.get(constNum).find(x => x.WorkNum === numVal && x.DetailWorkNum === detailVal);
+
+                curObject.MaterialUnit = Number(bidT3[key]['C16']['_text'].slice(1, -1));
+                curObject.LaborUnit = Number(bidT3[key]['C17']['_text'].slice(1, -1));
+                curObject.ExpenseUnit = Number(bidT3[key]['C18']['_text'].slice(1, -1));
+            }
+        }
+
+        Data.ExecuteReset = '0';
     }
 
     public static ApplyStandardPriceOption(): void {
@@ -308,27 +363,251 @@ export class CalculatePrice {
     public static SetExcludingPrice(): void {
         let TempInvestDirectSum: number = Data.Investigation['직공비'];
         let TempRealDirectSum: number = FillCostAccount.ToLong(Data.RealDirectMaterial + Data.RealDirectLabor + Data.RealOutputExpense); 
+        let InvestExSum: number = Data.ExcludingMaterial + Data.ExcludingLabor + Data.ExcludingExpense;
+        let TempExRate: number = Math.round((InvestExSum / TempInvestDirectSum) * 100000) / 100000;
+        let TempExPrice: number = Math.ceil(TempRealDirectSum * TempExRate);
+        let keyFound: number = 0;
+
+        const bidT3: object = this.eleBID['T3'];
+        let code: string;
+        let type: string;
+
+        if(Data.CostAccountDeduction.localeCompare('1')){
+            TempExPrice = Math.ceil(Math.ceil(TempExPrice * 0.997));
+        }
+
+        for (let key in bidT3) {
+            code = JSON.stringify(bidT3[key]['C9']['_text']);
+            type = JSON.stringify(bidT3[key]['C5']['_text'])[1];
+
+            if (code !== undefined && type === 'S') {
+                let constNum: string = JSON.stringify(bidT3[key]['C1']['_text']).slice(1, -1);
+                let numVal: string = JSON.stringify(bidT3[key]['C2']['_text']).slice(1, -1);
+                let detailVal: string = JSON.stringify(bidT3[key]['C3']['_text']).slice(1, -1);
+                let curObject = Data.Dic.get(constNum).find(x => x.WorkNum === numVal && x.DetailWorkNum === detailVal);
+
+                if (curObject.Item.localeCompare('제요율적용제외')) {
+                    if(this.maxBID === undefined){
+                        this.maxBID = bidT3[key];
+                    }
+
+                    if(bidT3[key]['C15']['_text'].slice(1, -1) === '1' && bidT3[key]['C15']['_text'].slice(1, -1) !== '0'){
+                        if(Number(bidT3[key]['C19']['_text'].slice(1, -1)) > Number(this.maxBID['C19']['_text'].slice(1, -1))){
+                            if(Number(bidT3[key]['C19']['_text'].slice(1, -1)) * 1.5 > curObject.PriceSum + (TempExPrice - this.exSum)){
+                                keyFound = 1;
+                                this.maxBID = bidT3[key];
+                            }
+                        }
+                    }
+
+                    bidT3[key]['C16']['_text'] = curObject.MaterialUnit.toString();
+                    bidT3[key]['C17']['_text'] = curObject.LaborUnit.toString();
+                    bidT3[key]['C18']['_text'] = curObject.ExpenseUnit.toString();
+                    bidT3[key]['C19']['_text'] = curObject.UnitPriceSum.toString();
+                    bidT3[key]['C20']['_text'] = curObject.Material.toString();
+                    bidT3[key]['C21']['_text'] = curObject.Labor.toString();
+                    bidT3[key]['C22']['_text'] = curObject.Expense.toString();
+                    bidT3[key]['C23']['_text'] = curObject.PriceSum.toString();
+                }
+            }
+        }
+
+        if(keyFound === 0){
+            let divisionPrice: number = Math.trunc((TempExPrice - this.exSum) / this.exCount);
+            let deficiency: number = Math.ceil((TempExPrice - this.exSum) - (divisionPrice * this.exCount));
+            let count: number = 0;
+
+            while(count !== this.exCount){
+                const bidT3: object = this.eleBID['T3'];
+                let code: string;
+                let type: string;
+
+                for (let key in bidT3) {
+                    code = JSON.stringify(bidT3[key]['C9']['_text']);
+                    type = JSON.stringify(bidT3[key]['C5']['_text'])[1];
+        
+                    if (code !== undefined && type === 'S') {
+                        let constNum: string = JSON.stringify(bidT3[key]['C1']['_text']).slice(1, -1);
+                        let numVal: string = JSON.stringify(bidT3[key]['C2']['_text']).slice(1, -1);
+                        let detailVal: string = JSON.stringify(bidT3[key]['C3']['_text']).slice(1, -1);
+                        let curObject = Data.Dic.get(constNum).find(x => x.WorkNum === numVal && x.DetailWorkNum === detailVal);
+        
+                        if (curObject.Item.localeCompare('제요율적용제외') && curObject.Quantity === 1) {
+                            if(curObject.LaborUnit !== 0){
+                                if(Number(bidT3[key]['C19']['_text'].slice(1, -1)) * 1.5 > (curObject.LaborUnit + divisionPrice)){
+                                    curObject.LaborUnit += divisionPrice;
+
+                                    bidT3[key]['C17']['_text'] = curObject.LaborUnit.toString();
+                                    bidT3[key]['C19']['_text'] = curObject.UnitPriceSum.toString();
+                                    bidT3[key]['C21']['_text'] = curObject.Labor.toString();
+                                    bidT3[key]['C23']['_text'] = curObject.PriceSum.toString();
+                                    count++;
+                                }
+
+                                if(count === this.exCount){
+                                    bidT3[key]['C17']['_text'] = (deficiency + curObject.LaborUnit).toString();
+                                    bidT3[key]['C19']['_text'] = (deficiency + curObject.UnitPriceSum).toString();
+                                    bidT3[key]['C21']['_text'] = (deficiency + curObject.Labor).toString();
+                                    bidT3[key]['C23']['_text'] = (deficiency + curObject.PriceSum).toString();
+                                    break;
+                                }
+                            }
+                            else{
+                                if(curObject.ExpenseUnit !== 0){
+                                    if(Number(bidT3[key]['C19']['_text'].slice(1, -1)) * 1.5 > (curObject.ExpenseUnit + divisionPrice)){
+                                        curObject.ExpenseUnit += divisionPrice;
+    
+                                        bidT3[key]['C18']['_text'] = curObject.ExpenseUnit.toString();
+                                        bidT3[key]['C19']['_text'] = curObject.UnitPriceSum.toString();
+                                        bidT3[key]['C22']['_text'] = curObject.Expense.toString();
+                                        bidT3[key]['C23']['_text'] = curObject.PriceSum.toString();
+                                        count++;
+                                    }
+    
+                                    if(count === this.exCount){
+                                        bidT3[key]['C18']['_text'] = (deficiency + curObject.ExpenseUnit).toString();
+                                        bidT3[key]['C19']['_text'] = (deficiency + curObject.UnitPriceSum).toString();
+                                        bidT3[key]['C22']['_text'] = (deficiency + curObject.Expense).toString();
+                                        bidT3[key]['C23']['_text'] = (deficiency + curObject.PriceSum).toString();
+                                        break;
+                                    }
+                                }
+                                else{
+                                    if(Number(bidT3[key]['C19']['_text'].slice(1, -1)) * 1.5 > (curObject.MaterialUnit + divisionPrice)){
+                                        curObject.MaterialUnit += divisionPrice;
+    
+                                        bidT3[key]['C16']['_text'] = curObject.MaterialUnit.toString();
+                                        bidT3[key]['C19']['_text'] = curObject.UnitPriceSum.toString();
+                                        bidT3[key]['C20']['_text'] = curObject.Material.toString();
+                                        bidT3[key]['C23']['_text'] = curObject.PriceSum.toString();
+                                        count++;
+                                    }
+    
+                                    if(count === this.exCount){
+                                        bidT3[key]['C18']['_text'] = (deficiency + curObject.MaterialUnit).toString();
+                                        bidT3[key]['C19']['_text'] = (deficiency + curObject.UnitPriceSum).toString();
+                                        bidT3[key]['C22']['_text'] = (deficiency + curObject.Material).toString();
+                                        bidT3[key]['C23']['_text'] = (deficiency + curObject.PriceSum).toString();
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if((keyFound === 1) && (this.exSum < TempExPrice)){
+            this.maxBID['C17']['_text'] = (Number(this.maxBID['C17']['_text'].slice(1, -1)) + TempExPrice - this.exSum).toString();
+            this.maxBID['C19']['_text'] = (Number(this.maxBID['C19']['_text'].slice(1, -1)) + TempExPrice - this.exSum).toString();
+            this.maxBID['C21']['_text'] = (Number(this.maxBID['C21']['_text'].slice(1, -1)) + TempExPrice - this.exSum).toString();
+            this.maxBID['C23']['_text'] = (Number(this.maxBID['C23']['_text'].slice(1, -1)) + TempExPrice - this.exSum).toString();
+        }
+
     }
 
     public static GetAdjustedExcludePrice(): void {
+        const bidT3: object = this.eleBID['T3'];
+        let code: string;
+        let type: string;
 
+        for (let key in bidT3) {
+            code = JSON.stringify(bidT3[key]['C9']['_text']);
+            type = JSON.stringify(bidT3[key]['C5']['_text'])[1];
+
+            if (code !== undefined && type === 'S') {
+                let constNum: string = JSON.stringify(bidT3[key]['C1']['_text']).slice(1, -1);
+                let numVal: string = JSON.stringify(bidT3[key]['C2']['_text']).slice(1, -1);
+                let detailVal: string = JSON.stringify(bidT3[key]['C3']['_text']).slice(1, -1);
+                let curObject = Data.Dic.get(constNum).find(x => x.WorkNum === numVal && x.DetailWorkNum === detailVal);
+
+                if (curObject.Item.localeCompare('제요율적용제외')) {
+                    Data.AdjustedExMaterial += Number(bidT3[key]['C20']['_text'].slice(1, -1))
+                    Data.AdjustedExLabor += Number(bidT3[key]['C21']['_text'].slice(1, -1))
+                    Data.AdjustedExExpense += Number(bidT3[key]['C22']['_text'].slice(1, -1))
+                }
+            }
+        }
     }
 
     public static SetPriceOfSuperConstruction(): void {
+        let firstConstruction: JSON = undefined;
+        let secondConstruction: JSON = undefined;
+        let thirdConstruction: JSON = undefined;
+
+        const bidT3: object = this.eleBID['T3'];
+        let code: string;
+        let type: string;
+
+        for (let key in bidT3) {
+            code = JSON.stringify(bidT3[key]['C9']['_text']);
+            type = JSON.stringify(bidT3[key]['C5']['_text'])[1];    
+
+            if (code !== undefined && type === 'G') {
+                if(bidT3[key]['C23']['_text'].slice(1, -1) === '0'){
+                    if(firstConstruction === undefined || bidT3[key]['C3']['_text'].slice(1, -1) === '0'){
+                        firstConstruction = bidT3[key];
+                        secondConstruction = undefined;
+                        thirdConstruction = undefined;
+                    }
+                    else if(bidT3[key]['C3']['_text'].slice(1, -1) === firstConstruction['C2']['_text'].slice(1, -1) && firstConstruction !== undefined){
+                        secondConstruction = bidT3[key];
+                        thirdConstruction = undefined;
+                    }
+                    else if(bidT3[key]['C3']['_text'].slice(1, -1) === secondConstruction['C2']['_text'].slice(1, -1) && secondConstruction !== undefined)
+                        thirdConstruction = bidT3[key];
+                }
+                else{
+                    firstConstruction = undefined;
+                    secondConstruction = undefined;
+                    thirdConstruction = undefined;
+                }
+            }
+            else if(code !== undefined && type === 'S'){
+                if(firstConstruction !== undefined){
+                    firstConstruction['C20']['_text'] = (Number(firstConstruction['C20']['_text'].slice(1, -1)) + Number(bidT3[key]['C20']['_text'].slice(1, -1))).toString();
+                    firstConstruction['C21']['_text'] = (Number(firstConstruction['C21']['_text'].slice(1, -1)) + Number(bidT3[key]['C21']['_text'].slice(1, -1))).toString();
+                    firstConstruction['C22']['_text'] = (Number(firstConstruction['C22']['_text'].slice(1, -1)) + Number(bidT3[key]['C22']['_text'].slice(1, -1))).toString();
+                    firstConstruction['C23']['_text'] = (Number(firstConstruction['C23']['_text'].slice(1, -1)) + Number(bidT3[key]['C23']['_text'].slice(1, -1))).toString();
+                }
+                if(secondConstruction !== undefined){
+                    secondConstruction['C20']['_text'] = (Number(secondConstruction['C20']['_text'].slice(1, -1)) + Number(bidT3[key]['C20']['_text'].slice(1, -1))).toString();
+                    secondConstruction['C21']['_text'] = (Number(secondConstruction['C21']['_text'].slice(1, -1)) + Number(bidT3[key]['C21']['_text'].slice(1, -1))).toString();
+                    secondConstruction['C22']['_text'] = (Number(secondConstruction['C22']['_text'].slice(1, -1)) + Number(bidT3[key]['C22']['_text'].slice(1, -1))).toString();
+                    secondConstruction['C23']['_text'] = (Number(secondConstruction['C23']['_text'].slice(1, -1)) + Number(bidT3[key]['C23']['_text'].slice(1, -1))).toString();
+                }
+                if(thirdConstruction !== undefined){
+                    thirdConstruction['C20']['_text'] = (Number(thirdConstruction['C20']['_text'].slice(1, -1)) + Number(bidT3[key]['C20']['_text'].slice(1, -1))).toString();
+                    thirdConstruction['C21']['_text'] = (Number(thirdConstruction['C21']['_text'].slice(1, -1)) + Number(bidT3[key]['C21']['_text'].slice(1, -1))).toString();
+                    thirdConstruction['C22']['_text'] = (Number(thirdConstruction['C22']['_text'].slice(1, -1)) + Number(bidT3[key]['C22']['_text'].slice(1, -1))).toString();
+                    thirdConstruction['C23']['_text'] = (Number(thirdConstruction['C23']['_text'].slice(1, -1)) + Number(bidT3[key]['C23']['_text'].slice(1, -1))).toString();
+                }
+            }
+        }
 
     }
 
     public static SetBusinessInfo(): void {
         const bidT1: object = this.eleBID['T1'];
 
-        // bidT1['C17']['_text'] = '0000';
-        // bidT1['C18']['_text'] = 'test';
-
-        fs.writeFileSync("C:\\\\Users\\joung\\OneDrive\\문서\\AutoBID" + "\\OutputDataFromBID.json", JSON.stringify(this.docBID));
+        bidT1['C17']['_text'] = Data.CompanyRegistrationNum;
+        bidT1['C18']['_text'] = Data.CompanyRegistrationName;
     }
 
     public static SubstitutePrice(): void {
+        const bidT5: object = this.eleBID['T5'];
 
+        for (let key in bidT5) {
+            if(bidT5[key]['C4']['_text'].slice(1 , -1) !== '이윤' && Data.Bidding[bidT5[key]['C4']['_text'].slice(1, -1)]){
+                bidT5[key]['C8']['_text'] = Data.Bidding[bidT5[key]['C4']['_text'].slice(1, -1)].toString();
+            }
+            else if(Data.Rate1[bidT5[key]['C4']['_text'].slice(1, -1)]){
+                bidT5[key]['C8']['_text'] = Data.Bidding[bidT5[key]['C4']['_text'].slice(1, -1)].toString();
+            }
+        }
+
+        fs.writeFileSync("C:\\\\Users\\joung\\OneDrive\\문서\\AutoBID" + "\\OutputDataFromBID.json", JSON.stringify(this.docBID));
     }
 
     public static CreateZipFile(xlsfiles: Array<string>): void {
@@ -346,12 +625,11 @@ export class CalculatePrice {
     }
 
     public static CreateFile(): void {
-        // CreateResultFile.Create();
+        CreateResultFile.Create();
         const files: Array<string> = fs.readdirSync("C:\\\\Users\\joung\\OneDrive\\문서\\AutoBID");
         const xlsFiles: Array<string> = files.filter(file => file.substring(file.length - 4, file.length).toLowerCase() === '.xls');
         this.CreateZipFile(xlsFiles);
     }
 }
 
-
-CalculatePrice.Calculation();
+// CalculatePrice.Calculation();
